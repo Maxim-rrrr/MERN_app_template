76a6d089e68cb5d398605758418f86b3adb681a1
Maxim-rrrr <Kewin.rrrr@gmail.com>
2021-08-28 14:41:28 +0300

    +: readme

--- modules/logger.js
+++ modules/logger.js
@@ -9,31 +9,46 @@ const myFormat = printf(({ level, message, timestamp }) => {
 });
 
 import 'winston-mongodb';
-const logger = createLogger({
-    transports: [
-        new transports.Console({
+
+let logger_transports_conf = []
+
+if (config.get("logger_console")) {
+    logger_transports_conf.push(new transports.Console({
             format: combine(
                 timestamp(),
                 colorize(),
                 myFormat
             )
-        }),
-        new transports.File({
-            filename: 'logs/logs.log',
-            format: combine(
-                timestamp(),
-                myFormat
-            )
-        }),
-        // new transports.MongoDB({
-        //     db: config.get('mongoUrl'),
-        //     options: {
-        //         useUnifiedTopology: true
-        //     },
-        //     collection: 'log',
-        //     format: combine(timestamp(), json())
-        // })
-    ]
+        })
+    )
+}
+
+if (config.get("logger_file")) {
+    logger_transports_conf.push(new transports.File({
+        filename: 'logs/logs.log',
+        format: combine(
+            timestamp(),
+            myFormat
+        )
+        })
+    )
+}
+
+if (config.get("logger_mongoDB")) {
+    logger_transports_conf.push(new transports.MongoDB({
+        db: config.get('mongoUrl'),
+        options: {
+            useUnifiedTopology: true
+        },
+        collection: 'log',
+        format: combine(timestamp(), json())
+    })
+    )
+}
+
+
+const logger = createLogger({
+    transports: logger_transports_conf
 })
 
 export default logger
